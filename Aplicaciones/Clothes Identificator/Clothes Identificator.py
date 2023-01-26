1d896be12f361d5dc8779084109ac84176a56a34
Francisco Javier Muñoz <105164506+Franmu14@users.noreply.github.com>
2023-01-25 22:24:28 -0300

    Update Clothes Identificator.py

--- Aplicaciones/Clothes Identificator/Clothes Identificator.py
+++ Aplicaciones/Clothes Identificator/Clothes Identificator.py
@@ -10,50 +10,39 @@ Original file is located at
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-#Descargar set de datos de Fashion MNIST de Zalando
+
 datos, metadatos = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
 
-#Imprimir los metadatos para ver que trae el set
 metadatos
 
-#Obtenemos en variables separadas los datos de entrenamiento (60k) y pruebas (10k)
 datos_entrenamiento, datos_pruebas = datos['train'], datos['test']
 
-#Etiquetas de las 10 categorias posibles
 nombres_clases = metadatos.features['label'].names
 
 nombres_clases
 
-#Funcion de normalizacion para los datos (Pasar de 0-255 a 0-1)
-#Hace que la red aprenda mejor y mas rapido
+
 def normalizar(imagenes, etiquetas):
   imagenes = tf.cast(imagenes, tf.float32)
   imagenes /= 255 #Aqui lo pasa de 0-255 a 0-1
   return imagenes, etiquetas
 
-#Normalizar los datos de entrenamiento y pruebas con la funcion que hicimos
 datos_entrenamiento = datos_entrenamiento.map(normalizar)
 datos_pruebas = datos_pruebas.map(normalizar)
 
-#Agregar a cache (usar memoria en lugar de disco, entrenamiento mas rapido)
-datos_entrenamiento = datos_entrenamiento.cache()
-datos_pruebas = datos_pruebas.cache()
 
-#Mostrar una imagen de los datos de pruebas, de momento mostremos la primera
 for imagen, etiqueta in datos_entrenamiento.take(1):
   break
-imagen = imagen.numpy().reshape((28,28)) #Redimensionar, cosas de tensores, lo veremos despues
+imagen = imagen.numpy().reshape((28,28))
 
 import matplotlib.pyplot as plt
 
-#Dibujar dibujar
 plt.figure()
 plt.imshow(imagen, cmap=plt.cm.binary)
 plt.colorbar()
 plt.grid(False)
 plt.show()
 
-#Dibujar mas
 plt.figure(figsize=(10,10))
 for i, (imagen, etiqueta) in enumerate(datos_entrenamiento.take(25)):
   imagen = imagen.numpy().reshape((28,28))
@@ -65,7 +54,7 @@ for i, (imagen, etiqueta) in enumerate(datos_entrenamiento.take(25)):
   plt.xlabel(nombres_clases[etiqueta])
 plt.show()
 
-#Crear el modelo
+
 modelo = tf.keras.Sequential([
   tf.keras.layers.Flatten(input_shape=(28,28,1)), #1 - blanco y negro
   tf.keras.layers.Dense(50, activation=tf.nn.relu),
@@ -73,39 +62,34 @@ modelo = tf.keras.Sequential([
   tf.keras.layers.Dense(10, activation=tf.nn.softmax) #Para redes de clasificacion
 ])
 
-#Compilar el modelo
+
 modelo.compile(
     optimizer='adam',
     loss=tf.keras.losses.SparseCategoricalCrossentropy(),
     metrics=['accuracy']
 )
 
-#Los numeros de datos en entrenamiento y pruebas (60k y 10k)
 num_ej_entrenamiento = metadatos.splits["train"].num_examples
 num_ej_pruebas = metadatos.splits["test"].num_examples
 
 print(num_ej_entrenamiento)
 print(num_ej_pruebas)
 
-#El trabajo por lotes permite que entrenamientos con gran cantidad de datos se haga de manera mas eficiente
 TAMANO_LOTE = 32
 
-#Shuffle y repeat hacen que los datos esten mezclados de manera aleatoria para que la red
-#no se vaya a aprender el orden de las cosas
+
 datos_entrenamiento = datos_entrenamiento.repeat().shuffle(num_ej_entrenamiento).batch(TAMANO_LOTE)
 datos_pruebas = datos_pruebas.batch(TAMANO_LOTE)
 
 import math
 
-#Entrenar
 historial = modelo.fit(datos_entrenamiento, epochs=5, steps_per_epoch= math.ceil(num_ej_entrenamiento/TAMANO_LOTE))
 
-#Ver la funcion de perdida
 plt.xlabel("# Epoca")
 plt.ylabel("Magnitud de pérdida")
 plt.plot(historial.history["loss"])
 
-#Pintar una cuadricula con varias predicciones, y marcar si fue correcta (azul) o incorrecta (roja)
+
 import numpy as np
 
 for imagenes_prueba, etiquetas_prueba in datos_pruebas.take(1):
@@ -154,25 +138,5 @@ for i in range(num_imagenes):
   plt.subplot(filas, 2*columnas, 2*i+2)
   graficar_valor_arreglo(i, predicciones, etiquetas_prueba)
 
-#Probar una imagen suelta
-imagen = imagenes_prueba[4] #AL ser la variable imagenes_prueba solo tiene lo que se le puso en el bloque anterior heheh
-imagen = np.array([imagen])
-prediccion = modelo.predict(imagen)
 
 print("Prediccion: " + nombres_clases[np.argmax(prediccion[0])])
-
-#Exportacion del modelo a h5
-modelo.save('modelo_exportado.h5')
-
-#Instalar tensorflowjs para convertir el h5 a un modelo que pueda cargar tensorflowjs en un explorador
-!pip install tensorflowjs
-
-#Convertir el archivo h5 a formato de tensorflowjs
-!mkdir tfjs_target_dir
-!tensorflowjs_converter --input_format keras modelo_exportado.h5 tfjs_target_dir
-
-#Veamos si si creo la carpeta
-!ls
-
-#Veamos el contenido de la carpeta
-!ls tfjs_target_dir
